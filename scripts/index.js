3105484d2fda69081bbb31a8ee911cba58061f44
antexibrahim <33679079+antexibrahim@users.noreply.github.com>
2021-06-25 03:50:28 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Jul 28, 2021',
+  name: 'Uyay Bucudu'
 };
 
 function hideEverything() {
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['HAPPY', 'BIRTHDAY!', config.name],
+      strings: ['SELAMAT', 'ULANGTAHUN!', config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
